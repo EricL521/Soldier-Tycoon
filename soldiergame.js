2fc7bce95f16058261faf1eb3bbefa8468cbf5bb
GCreeper00 <51763643+GCreeper00@users.noreply.github.com>
2019-11-27 12:31:15 -0500

    Bug Fix

--- soldiergame.js
+++ soldiergame.js
@@ -210,7 +210,7 @@ if (getCookie("data") !== "") {
 	}
 	
 } else {
-	setCookie("data", JSON.stringify({gold: gold, soldiers: soldiers.length, miners: miners.length, 
+	setCookie("data", JSON.stringify({gold: gold, soldiers: soldiers.length, miners: miners.length,
 																		soldierUpgradeCost: soldierUpgradeCost, soldierHealth: soldierHealth, 
 																		minSoldierDamage: minSoldierDamage, minerUpgradeCost: minerUpgradeCost, 
 																	 	timerPerGold: timePerGold, castleGPS: castle.castleGPS, maxPeople: maxPeople, 
